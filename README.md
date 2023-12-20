4afcaf3bac1418d6e93ce81c9d0f7a8a08ccd3bf
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-21 01:41:51 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ứng dụng nhận dạng ảnh chữ số viết tay.
+## Link web: https://21520223-bai05.streamlit.app
---
+++ main.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from PIL import Image
+import pickle as pkl
+import numpy as np
+
+with open("styles.css") as f:
+    custom_css = f.read()
+st.markdown(f"<style>{custom_css}</style>", unsafe_allow_html=True)
+st.title('Handwritten Digit Recognition')
+
+st.image("image.gif", use_column_width=True)
+input = open('lrc_mnist.pkl', 'rb')
+model = pkl.load(input)
+
+st.header('Upload handwritten digit image')
+uploaded_file = st.file_uploader("Choose an image file", type=(['png', 'jpg', 'jpeg']))
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Test image')
+
+    if st.button('Predict'):
+        image = image.resize((8*8, 1))
+        feature_vector = np.array(image)
+        label = str((model.predict(feature_vector))[0])
+
+        st.header('Result')
+        st.text(label)
